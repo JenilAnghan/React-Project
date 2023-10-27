0e0d0a30cddb9d015acbc0686fd498a7fcdbb618
jenil <jenilanghan508@gmail.com>
2023-10-27 12:01:23 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 // import viteLogo from '/vite.svg'
 import "./App.css";
 
-return <></>;
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
 
 export default App;
