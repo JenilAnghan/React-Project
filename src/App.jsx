9fbf9f579a5c04e0bc090608d8605a036f8c3272
jenil <jenilanghan508@gmail.com>
2023-10-27 21:37:34 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
+import { Card } from "reactstrap";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  return <></>;
+  return <>
+  <Card/>
+  </>;
 }
 
 export default App;
