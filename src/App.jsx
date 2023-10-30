26cc47521dce737a4adc19987090f862cd75a020
jenil <jenilanghan508@gmail.com>
2023-10-30 10:28:40 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import "./App.css";
-import { Card } from "reactstrap";
-
+import Cardss from "./Components/Cardss.jsx"
+import UseStaeCom from "./UseState/UsestateFun";
 function App() {
-  // const [count, setCount] = useState(0);
 
   return <>
-  <Card/>
+
+  <Cardss/>
+  <UseStaeCom/>
+
   </>;
 }
 
